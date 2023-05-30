5777444fed4b620427a920b94ea1d1c1f37264e5
guilhermedel <glhrmgithub@gmail.com>
2023-05-30 20:02:15 -0300

    Fazendo a funçao de lerarquivo

---
+++ CompiladorAssembly/Controller/Compilador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiladorAssembly.Controller
+{
+    class Compilador
+    {
+        public Dictionary<int,dynamic> Lista { get; set; } 
+        public void LerArquivo()
+        {
+            var line = File.ReadAllLines("./Teste.txt");
+            for (int i = 0; i < line.Length; i++)
+            {
+                foreach (var item in line[i])
+                {
+                    Lista.Add(i, item);
+                }
+            }
+        }
+    }
+}
--- CompiladorAssembly/Program.cs
+++ CompiladorAssembly/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CompiladorAssembly.Controller;
+
+namespace CompiladorAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compilador compilador= new Compilador();
+            compilador.LerArquivo();
+        }
+    }
+}
